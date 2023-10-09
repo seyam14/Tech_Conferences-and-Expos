1227393b90b8d092aa0a7318152785cc4e5578a2
seyam <mdseyam3215@gmail.com>
2023-10-09 22:18:13 +0600

    livelink and features add in readme file

---
+++ README.md
@@ -0,0 +1,19 @@
+[Live link](https://tech-conferences-and-expos.web.app)
+
+
+# some  features for this Tech_Conferences and Expos
+
+## Tech_Conferences and Expos Project
+
+## 1. Services
+ project services for tech conferences and expos.understanding the unique needs of tech events and are committed to ensuring their success
+
+## 2. Events
+The "Tech Conferences and Expos" project aims to create a comprehensive platform for managing and promoting technology-related events such as conferences, expos, seminars, and workshops
+## 3. Firebase connection and deploy
+Firebase documentation for detailed instructions on setting up and configuring Firebase services for  specific project requirements.
+
+## speakers time and topic 
+## login and registration etc 
+
+
--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -2,84 +2,85 @@ import { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../Firebase/AuthProvider";
 
-
 const Header = () => {
   const { user, logout } = useContext(AuthContext);
-  console.log(user);
+
   const handleLogout = () => {
     logout().then((result) => console.log(result));
   };
-    const links = (
-        <>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Services">Services</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Events">Events</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Speakers">Speakers</NavLink>
-          </li>
-          
-        </>
-      );
-    
-      return (
-        <div className="navbar bg-base-100">
-          <div className="navbar-start">
-            <div className="dropdown">
-              <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
-                  />
-                </svg>
-              </label>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-              >
-                {links}
-              </ul>
-            </div>
-            <a className="btn btn-ghost normal-case text-xl">Tech_Conferences and Expos</a>
-          </div>
-          <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-              {links}</ul>
-          </div>
-          <div className="navbar-end">
-          {user && (
-          <button className="btn btn-primary" onClick={handleLogout}>
-            logout
-          </button>
-  
-             )}
-             <div className="gap-1 m-2">
-             <p>{user?.displayName}</p>
-             </div>
-             <div className="gap-1 m-2">
-             <img className="w-10 rounded-full" src={user?.photoURL} alt="" />
-             </div>
-              
-                <Link to="/Login">
-                    <button className="btn">Login</button>
-                </Link>
-            </div>
+
+  const links = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/Services">Services</NavLink>
+      </li>
+      <li>
+        <NavLink to="/Events">Events</NavLink>
+      </li>
+      <li>
+        <NavLink to="/Speakers">Speakers</NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {links}
+          </ul>
         </div>
-      );
-    };
+        <a className="btn btn-ghost normal-case text-xl">
+          Tech_Conferences and Expos
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="navbar-end">
+        {user ? (
+          <>
+            <button className="btn btn-primary" onClick={handleLogout}>
+              Logout
+            </button>
+            <div className="gap-1 m-2">
+              <p>{user.displayName}</p>
+            </div>
+            <div className="gap-1 m-2">
+              <img className="w-10 rounded-full" src={user.photoURL} alt="" />
+            </div>
+          </>
+        ) : (
+          <Link to="/Login">
+            <button className="btn">Login</button>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
